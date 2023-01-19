ef0eca6e2c442a2a9d9f1de8be7dfb108f4f570a
Andy Gratton <agratton2988@googlemail.com>
2023-01-19 15:40:22 +0000

    First Commit

---
+++ Naverisk.psm1
@@ -0,0 +1,39 @@
+﻿Function Start-Naverisk{
+    Start-Service -Name NaveriskAgent
+    Start-Service -Name NaveriskServiceMonitor
+
+    Get-Service -Name nav*
+}
+
+Function Stop-Naverisk{
+    Stop-Service -Name NaveriskAgent
+    Stop-Service -Name NaveriskServiceMonitor
+
+    Get-Service -Name nav*
+}
+
+Function Restart-Naverisk{
+    Restart-Service -Name NaveriskAgent
+    Restart-Service -Name NaveriskServiceMonitor
+
+    Get-Service -Name nav*
+}
+
+Function Show-Naverisk{
+    Get-Service -Name nav*
+}
+
+function Get-Naverisk{
+    Write-Host "
+    Start-Naverisk
+    Stop-Naverisk
+    Restart-Naverisk
+    Show-Naverisk
+    "
+}
+
+Write-Host "
+Naverisk Module Connected
+
+To Get all Commands Type Get-Naverisk
+"
